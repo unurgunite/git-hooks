ff0df455f94f7a51c9817d381f92113801cccf2b
unurgunite <senpaiguru1488@gmail.com>
2022-06-11 21:25:35 +0300

    Added skeleton for `commit-msg` hook

---
+++ commit-msg.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def edit_commit_message
+  # COMMENT ABOVE
+  file = ARGV[0]
+  # File.open(file) do |l|
+  # end
+  # sed -i.bak -e "1s/.*/#$ticket_id $ticket_title/" "$file"
+end
--- commit-msg.sh
+++ commit-msg.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 #|-----------------------------------------------------------------------------------------------------------------|
-#| Program Name: pre-commit.sh
+#| Program Name: commit-msg.sh
 #|-----------------------------------------------------------------------------------------------------------------|
-#| Description: This script is a standard git hook. It checks for syntax of commited files and runs RuboCop to
-#|                improve it. This script uses POSIX standards and should work with no errors in Unix as well as in
+#| Description: This script is a standard git hook. It changes commit message according to local standards. This 
+#|                script uses POSIX standards and should work with no errors in Unix as well as in
 #|                other Unix-like OS'.
 #|-----------------------------------------------------------------------------------------------------------------|
 #| Description: This script checks for syntax of commited files
@@ -18,4 +18,30 @@
 #|-----------------------------------------------------------------------------------------------------------------|
 #| License: MIT
 #|-----------------------------------------------------------------------------------------------------------------|
+
+./commit-msg.rb
+
+start_with_verb(message) {
+	echo 'Analyzing message...'
+	case $message in
+		Added*|\
+		Created*|\
+		Fixed*|\
+		Updated*|\
+		Reworked*|\
+		Removed*)
+			echo 'Done!'
+			return 0
+			;;
+		*)
+			echo "The commit message should start with one of this words or with verb in past simple tense"
+			return 1
+			;;
+	esac
+}
+
+change_commit() {
+	file="$1"
+	sed -i.bak -e "1s/.*/#$ticket_id $ticket_title/" "$file"
+}
 
